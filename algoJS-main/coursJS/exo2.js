7792aa322626d3433b58a0c621a04532edfa15d8
crocmyto <73654393+crocmyto@users.noreply.github.com>
2023-10-13 01:32:40 +0200

    Ajout exo taxi

---
+++ algoJS-main/coursJS/exo2.js
@@ -0,0 +1,41 @@
+/*const a = "Jean"
+const b = "Paul"
+const result = checkname(a,b)
+
+function checkname(a,b){
+    if(a == b){
+        return "a et b sont identiques"
+    } else {
+        return "a est différent de b"
+    }
+}
+console.log(result)
+
+let names = []
+names.push("Vincent","Paul","Arthur")
+names.forEach((name) => {
+    console.log(name + " va à la pêche")
+})*/
+
+/*let student = {
+    studName:"Yannis", 
+    favoriteFood:"Sandwich",
+    city:"Carrière-sur-Seine",
+}
+let a = 0
+Object.values(student).forEach((nom) =>{
+    a += nom.length
+})
+if (a%2==0){
+    console.log("Pair")
+}else {
+    console.log("Impaire")
+}
+console.log(a)*/
+
+
+/*if (((Object.values(student.studName).length+Object.values(student.favoriteFood).length+Object.values(student.city).length)%2) < 1) {
+    console.log("pair")
+}else {
+    console.log("impair")
+}*/
