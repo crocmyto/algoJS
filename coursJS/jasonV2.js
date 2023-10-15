9ca49a8a886b829caecbb1c975ec1c8718c4c763
crocmyto <73654393+crocmyto@users.noreply.github.com>
2023-10-15 21:49:44 +0200

    JasonV2Fini

--- coursJS/jasonV2.js
+++ coursJS/jasonV2.js
@@ -4,12 +4,12 @@ let Jason = {
 }
 
 const GenNom = () => {
-    const noms = ["nom1","nom2","nom3","nom4","nom5","nom6","nom7"]
+    const noms = ["Michel ","Waren ","Toméo ","Sardoche ","Jean-Eude ","Charles ","Ben Laden ","Fitipaldi ","Fangio ","Rodri "]
     return noms[Math.floor(Math.random() * noms.length)]
 }
 
 const GenCara = () => {
-    const cara = ["cara1","cara2","car3","cara4","cara5","cara6","cara7",]
+    const cara = ["Intello","Crack-Head","Dépressif","Trisomique","Paranoiaque","Schizophrene","Arabe","Pervers","Narcissique","Peureux"]
     return cara[Math.floor(Math.random() * cara.length)]
 };
 
@@ -18,29 +18,28 @@ let survivants = [];
 for (let i = 0; i < 5; i++) {
     let survivant = {
         nom: GenNom(),
-        caracteristique: GenCara(),
+        cara: GenCara(),
         enVie: true
     }
     survivants.push(survivant)
 }
 const attaquer = (attaquant, cible) => {
-    let ProbMourir = 0.3
-    let ProbDmg = 0.5
-    let ProbMortDmg = 0.2
-
-    let rand = Math.random()
-    if (rand < ProbMourir) {
-        cible.enVie = false
-        console.log(attaquant.nom + " a tué " + cible.nom)
-    } else if (rand < ProbDmg) {
+    let ProbMourir = 3
+    let ProbDmg = 5
+    //let ProbMortDmg = 2
+    let rand = Math.floor(Math.random() * 11)
+    if (rand <= ProbMourir) {
+        cible.enVie = false;
+        console.log(attaquant.nom + " a tué " + cible.nom + cible.cara);
+    } else if (rand <= ProbDmg + ProbMourir) {
         attaquant.pointsDeVie -= 10;
-        console.log(cible.nom + " a esquivé et a infligé 10 points de dégâts à " + attaquant.nom)
-    } else if (rand < ProbMortDmg) {
-        cible.enVie = false
-        attaquant.pointsDeVie -= 15
-        console.log(cible.nom + " a infligé 15 points de dégâts à " + attaquant.nom + " mais est mort")
+        console.log(cible.nom + cible.cara + " a esquivé et a infligé 10 points de dégâts à " + attaquant.nom);
+    } else if (rand <= 10) {
+        cible.enVie = false;
+        attaquant.pointsDeVie -= 15;
+        console.log(cible.nom + cible.cara + " a infligé 15 points de dégâts à " + attaquant.nom + " mais est mort");
     }
-};
+}
 
 while (Jason.pointsDeVie > 0 && survivants.some(s => s.enVie)) {
     let randomSurvivant = survivants.filter(s => s.enVie)[Math.floor(Math.random() * survivants.filter(s => s.enVie).length)]
@@ -49,8 +48,8 @@ while (Jason.pointsDeVie > 0 && survivants.some(s => s.enVie)) {
 let ListMort = []
 console.log("Morts : ")
 survivants.filter(s => !s.enVie).forEach(mort => {
-    console.log(mort.nom)
-    ListMort.push(mort.nom)
+    console.log(mort.nom + mort.cara)
+    ListMort.push(mort.nom + mort.cara)
 })
 
 if (Jason.pointsDeVie <= 0) {
